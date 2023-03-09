2303883359d2346023ad71c9d5da20c6d2ce6384
Marc Joseph Ramos <marc_ramos@dlsu.edu.ph>
2023-03-09 21:03:02 +0800

    initialization w/ problem on file

---
+++ loop_performance.py
@@ -0,0 +1,61 @@
+""""
+General Objective
+
+    Create a function that calculates the sum of all numbers from 1 to n using
+        For Loop
+        While Loop
+
+    Import the time module
+        Measure the execution time of each function
+        Call the functions using different values of n (10, 100, 1,000, 10,000, 100,000)
+        Record the execution time
+
+Based on the experiment, which of the two loops performs much faster?
+
+ 
+
+Here's a python code you can use to measure the execution time of a function
+
+import time
+
+n = 1000
+
+time_start = time.time()
+
+for_loop_function(n)
+
+time_end = time.time()
+
+time_total = time_end - time_start
+
+print(time_total)General Objective
+
+    Create a function that calculates the sum of all numbers from 1 to n using
+        For Loop
+        While Loop
+
+    Import the time module
+        Measure the execution time of each function
+        Call the functions using different values of n (10, 100, 1,000, 10,000, 100,000)
+        Record the execution time
+
+Based on the experiment, which of the two loops performs much faster?
+
+ 
+
+Here's a python code you can use to measure the execution time of a function
+
+import time
+
+n = 1000
+
+time_start = time.time()
+
+for_loop_function(n)
+
+time_end = time.time()
+
+time_total = time_end - time_start
+
+print(time_total)
+"""
